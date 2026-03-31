0bd7909ab7370188dc617915c54411fe528cd151
iszackthomas3-spes <iszackthomas3@gmail.com>
2026-03-31 22:59:15 +0000

    Simple page: Join Our Discord

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,24 @@
-import React, { useMemo, useState } from 'react';
+export default function Home() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      minHeight: '100vh',
+      backgroundColor: '#0f0f0f',
+      margin: 0,
+      fontFamily: 'Arial, sans-serif'
+    }}>
+      <h1 style={{
+        fontSize: '48px',
+        color: '#ffffff',
+        textAlign: 'center'
+      }}>
+        Join Our Discord
+      </h1>
+    </div>
+  );
+}
 
 async function fetchJson(url, options = {}) {
   const res = await fetch(url, options);
